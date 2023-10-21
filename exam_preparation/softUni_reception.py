0e85e0ccb9bb3f8dd59f1f24ce1c9d1787f55798
Vanya Voivodova <145048073+voivodova@users.noreply.github.com>
2023-10-21 16:04:25 +0300

    Update softUni_reception.py

--- exam_preparation/softUni_reception.py
+++ exam_preparation/softUni_reception.py
@@ -1,13 +1,15 @@
-employee1_efficiency = int(input())
-employee2_efficiency = int(input())
-employee3_efficiency = int(input())
+employee1 = int(input())
+employee2 = int(input())
+employee3 = int(input())
 students_count = int(input())
 
-sum_of_ee = employee1_efficiency + employee2_efficiency + employee3_efficiency
-hours = students_count // sum_of_ee
-if students_count % sum_of_ee != 0:
-    hours += 1
-for h in range(1, hours + 1):
-    if h % 4 == 0:
-        hours += 1
-print(f"Time needed: {hours}h.")
+students_per_hour = employee1 + employee2 + employee3
+total_time = 0
+
+while students_count > 0:
+    total_time += 1
+    if total_time % 4 == 0:
+        total_time += 1
+    students_count -= students_per_hour
+
+print(f"Time needed: {total_time}h.")
